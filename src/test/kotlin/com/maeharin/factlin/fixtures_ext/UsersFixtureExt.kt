956a76545372c73376ede44418a69be35e50f072
maeharin <maeharin@gmail.com>
2018-08-05 18:31:40 +0900

    add fixture ext

---
+++ src/test/kotlin/com/maeharin/factlin/fixtures_ext/UsersFixtureExt.kt
@@ -0,0 +1,7 @@
+package com.maeharin.factlin.fixtures_ext
+
+import com.maeharin.factlin.fixtures.UsersFixture
+
+fun UsersFixture.default() = this.copy(job = "エンジニア", age = 30)
+fun UsersFixture.active() = this.default().copy(status = "ACTIVE")
+fun UsersFixture.inActive() = this.default().copy(status = "IN_ACTIVE")
--- src/test/kotlin/factlin_sample/SampleTest.kt
+++ src/test/kotlin/factlin_sample/SampleTest.kt
@@ -2,6 +2,8 @@ package factlin_sample
 
 import com.maeharin.factlin.fixtures.UsersFixture
 import com.maeharin.factlin.fixtures.insertUsersFixture
+import com.maeharin.factlin.fixtures_ext.active
+import com.maeharin.factlin.fixtures_ext.inActive
 import com.ninja_squad.dbsetup.destination.DriverManagerDestination
 import com.ninja_squad.dbsetup_kotlin.dbSetup
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -19,13 +21,25 @@ class SampleTest {
     fun testInsertUser() {
         dbSetup(dest) {
             deleteAllFrom(listOf("users"))
-            insertUsersFixture(UsersFixture())
+            insertUsersFixture(UsersFixture().active().copy(id = 1, name = "テスト1"))
+            insertUsersFixture(UsersFixture().inActive().copy(id = 2, name = "テスト2"))
         }.launch()
 
         val stmt = dest.connection.createStatement()
-        val rs = stmt.executeQuery("select * from users")
+        val rs = stmt.executeQuery("select * from users order by id asc")
 
         assertTrue(rs.next())
-        assertEquals("", rs.getString("name"))
+        assertEquals("1", rs.getString("id"))
+        assertEquals("テスト1", rs.getString("name"))
+        assertEquals("ACTIVE", rs.getString("status"))
+        assertEquals("エンジニア", rs.getString("job"))
+        assertEquals("30", rs.getString("age"))
+
+        assertTrue(rs.next())
+        assertEquals("2", rs.getString("id"))
+        assertEquals("テスト2", rs.getString("name"))
+        assertEquals("IN_ACTIVE", rs.getString("status"))
+        assertEquals("エンジニア", rs.getString("job"))
+        assertEquals("30", rs.getString("age"))
     }
 }
